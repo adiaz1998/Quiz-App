c57a1468c3d0ff0d8221bfea4f5c99dcbef2b0ea
Alan Diaz <adiaz98.2020@gmail.com>
2021-11-10 16:34:17 -0500

    Class Exit

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -2,18 +2,59 @@
 //  ViewController.swift
 //  Quiz App
 //
-//  Created by Alan Díaz on 11/10/21.
+//  Created by Alan Díaz on 11/02/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Outlers -> Variable | // Actions -> Functions
+    
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var questions: UILabel!
+    
+    var questionBank = [["5 + 2 = 9", "False"],
+                        ["9 - 2 = 8", "False"],
+                        ["2 + 3 = 5", "True"],
+                        ["1 - 1 = -1", "False"]]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI() // Calling a function
+        
+    }
+    
+    func updateUI(){
+        
+        questions.text = questionBank[0]
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    
+        userInput = sender.currentTitle!
+        
+        if userInput == AnswerBank{
+            
+            sender.backgroundColor = green
+            
+        }
+        
+        else{
+        sender.backgroundColor = red
+        }
+        
+        question += 1
+        
+    }
+    
 }
 
